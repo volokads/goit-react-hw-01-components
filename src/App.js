6a6a35160e74c4ec2ed1ed96272d1ddec5d62f22
volokads <hluhovspg@gmail.com>
2021-10-31 11:38:39 +0200

    begining

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>lesson 1</h1>
+      
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In impedit repudiandae praesentium a placeat aut sit commodi voluptatum aliquid labore? Quaerat, reiciendis! Eos veritatis distinctio modi! Obcaecati consectetur molestiae dolore.</p>
+    </div>
+  );
+}
+
+export default App;
