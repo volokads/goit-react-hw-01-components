53bb3521f315afef8eb66a1ae6cdc825bf84d7c4
volokads <hluhovspg@gmail.com>
2022-02-07 18:30:25 +0200

    title

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,35 +1,36 @@
 import PropTypes from 'prop-types'
 import s from './Statistics.module.css'
 
-function Statistic({ stats }) {
+function Statistic({ title, stats }) {
     let total = 0
     function newPar() {
-        
+
         stats.map((stat) => {
             if (stat.label) {
-            return    total += stat.percentage
+                return total += stat.percentage
             }
-          return  total
+            return total
         })
-        return 
+        return
     }
     newPar()
-        
+
 
 
     return (
         <section className={s.statistics}>
-            <h2 className={s.title}>Upload stats</h2>
-        <ul className={s.statList}>
-             {stats.map((stat) => {
-                return (
-                    <li className={s.item} key={stat.id} style={{ backgroundColor: `rgb(${listItemColor()})`}}>
-                    <span className={s.label}>{stat.label}</span>
-                    <span className={s.percentage}>{stat.percentage}</span>
-                    </li>)
-            })}
-  </ul>
-</section>
+            {title ? null : <h2 className={s.title}>Upload stats</h2>}
+            {/* <h2 className={s.title}>Upload stats</h2> */}
+            <ul className={s.statList}>
+                {stats.map((stat) => {
+                    return (
+                        <li className={s.item} key={stat.id} style={{ backgroundColor: `rgb(${listItemColor()})` }}>
+                            <span className={s.label}>{stat.label}</span>
+                            <span className={s.percentage}>{stat.percentage}</span>
+                        </li>)
+                })}
+            </ul>
+        </section>
     )
 }
 
@@ -40,10 +41,10 @@ const listItemColor = () => {
 }
 
 const randomColor = () => {
-    return Math.floor(Math.random()*255)
+    return Math.floor(Math.random() * 255)
 }
 
 Statistic.propTypes = {
-  title: PropTypes.string,
-  stats: PropTypes.array
+    title: PropTypes.string,
+    stats: PropTypes.array
 }
